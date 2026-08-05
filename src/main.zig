6aeb0d01749f8e351450bfdb61401739387f0a3a
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-05 16:45:55 +0200

    chore: if statement and while loop

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,12 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    stdout("Hello Zig", .{});
+    var compter: i32 = 10;
+
+    while (compter >= 0) {
+        if (compter > 0) {
+            stdout("Warning {d} \n", .{compter});
+        } else stdout("Boum ! \n", .{});
+        compter -= 1;
+    }
 }
