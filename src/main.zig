f578a3bae4c349e2351f5ba9418908f1369300b8
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-05 16:51:42 +0200

    chore: functions

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,19 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    var compter: i32 = 10;
+    stdout("Score: {d} \n", .{add(1, 2)});
+    stdout("2 is odd {} \n", .{isOdd(2)});
+    stdout("2 is even {} \n", .{isEven(2)});
+}
 
-    while (compter >= 0) {
-        if (compter > 0) {
-            stdout("Warning {d} \n", .{compter});
-        } else stdout("Boum ! \n", .{});
-        compter -= 1;
-    }
+fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+fn isOdd(n: u32) bool {
+    return n % 2 == 0;
+}
+
+fn isEven(n: u32) bool {
+    return !isOdd(n);
 }
