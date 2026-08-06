39104518bdf1e66b669a07545722e14e17428ff6
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-06 14:20:48 +0200

    chore: struct

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,15 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    const prices = [_]f32{ 5.50, 2.30, 1.90 };
-    stdout("Total price {} \n", .{total(&prices)}); // <- pass pointer ref
+    const john = User{ .first_name = "John", .last_name = "Doe" };
+    try john.greeting();
 }
 
-fn total(xs: []const f32) f32 {
-    var output: f32 = 0;
-    for (xs) |x| output += x;
-    return output;
-}
+const User = struct {
+    first_name: []const u8,
+    last_name: []const u8,
+
+    pub fn greeting(self: User) !void {
+        stdout("Hello {s} {s} !\n", .{ self.first_name, self.last_name });
+    }
+};
