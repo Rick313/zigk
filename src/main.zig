38d1a2952a7a425c89651bb940177a0a900c985d
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-07 17:39:01 +0200

    chore: allocator

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,21 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    // Defer is used to execute a statement upon exiting the current block.
-    defer stdout("Good bye dude !\n", .{});
-    stdout("Welcome Zig !\n", .{});
-    stdout("This is a short program written in zig.\n", .{});
+    // 1. create allocator
+    // 2. try to allocate 5
+    // 3. free memory
+    // 4. update value
+    // 5. print result
+
+    const pa = std.heap.page_allocator;
+    var list = try pa.alloc(i32, 5);
+    defer pa.free(list);
+    list[0] = 42;
+    stdout("The answer is {d}! \n", .{list[0]});
+
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    const bytes = try allocator.dupe(u8, "Hello zig allocator !");
+    defer allocator.free(bytes);
+    stdout("{s}\n", .{bytes});
 }
