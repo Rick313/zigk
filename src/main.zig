ffdba4195f0f21b1562f50aa379002261e9b19e1
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-07 17:28:04 +0200

    chore: defer keyword

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,8 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    // catch case
-    var a = div(12, 0) catch |err| switch (err) {
-        DivError.DivByZero => 0.0,
-        else => |e| return e, // propagate
-    };
-    stdout("Result {d}\n", .{a});
-
-    // catch default
-    a = div(12, 0) catch 0.0;
-    stdout("Result {d}\n", .{a});
-}
-
-const DivError = error{DivByZero};
-
-fn div(numerator: f32, denominator: f32) DivError!f32 {
-    if (denominator == 0) return DivError.DivByZero;
-    return numerator / denominator;
+    // Defer is used to execute a statement upon exiting the current block.
+    defer stdout("Good bye dude !\n", .{});
+    stdout("Welcome Zig !\n", .{});
+    stdout("This is a short program written in zig.\n", .{});
 }
