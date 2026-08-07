7e5f21fd84599254418341e001bcd17179767df5
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-07 17:24:35 +0200

    chore: errors and catch errors

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,21 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    var john = Hero{ .name = "John", .life_pt = 10, .attack_pt = 2 };
-    var bob = Hero{ .name = "Bob", .life_pt = 12, .attack_pt = 1 };
-    john.attack(&bob);
-    try bob.showLife();
+    // catch case
+    var a = div(12, 0) catch |err| switch (err) {
+        DivError.DivByZero => 0.0,
+        else => |e| return e, // propagate
+    };
+    stdout("Result {d}\n", .{a});
+
+    // catch default
+    a = div(12, 0) catch 0.0;
+    stdout("Result {d}\n", .{a});
 }
 
-const Hero = struct {
-    name: []const u8,
-    life_pt: u32,
-    attack_pt: u32,
-    pub fn attack(self: Hero, target: *Hero) void {
-        stdout("[{s}] Attack {s} ! \n", .{ self.name, target.name });
-        target.life_pt -= self.attack_pt;
-    }
-    pub fn showLife(self: Hero) !void {
-        stdout("[{s}] My life point is {d}. \n", .{ self.name, self.life_pt });
-    }
-};
+const DivError = error{DivByZero};
+
+fn div(numerator: f32, denominator: f32) DivError!f32 {
+    if (denominator == 0) return DivError.DivByZero;
+    return numerator / denominator;
+}
