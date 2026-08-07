64e740cc607f6fb5fc5cd74ef4b03f945b6d1177
RAFAIDEEN Richard <richard.rafaideen@soregies.fr>
2026-08-07 18:14:27 +0200

    chore: slices []const u8

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,11 @@ const std = @import("std");
 const stdout = std.debug.print;
 
 pub fn main() !void {
-    // 1. create allocator
-    // 2. try to allocate 5
-    // 3. free memory
-    // 4. update value
-    // 5. print result
+    const txt1 = "Hello";
+    const txt2 = "Zig !";
 
-    const pa = std.heap.page_allocator;
-    var list = try pa.alloc(i32, 5);
-    defer pa.free(list);
-    list[0] = 42;
-    stdout("The answer is {d}! \n", .{list[0]});
+    const msg =
+        if (std.mem.eql(u8, txt1, txt2)) "It's the same bytes !" else "They're differents !";
 
-    var gpa = std.heap.DebugAllocator(.{}).init;
-    const allocator = gpa.allocator();
-    const bytes = try allocator.dupe(u8, "Hello zig allocator !");
-    defer allocator.free(bytes);
-    stdout("{s}\n", .{bytes});
+    stdout("{s}\n", .{msg});
 }
